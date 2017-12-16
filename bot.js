636bd9ccea6f5ac47dc514d53b3c659973b8230c
PokemonAdventuresDevs <34608835+PokemonAdventuresDevs@users.noreply.github.com>
2017-12-16 15:03:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,20 @@ client.on('ready',() => {
 });
 
 var prefix = "a!"
-
+var answers = [
+	  "Did you know that this bot was made because of a friend of Jorge called Luca because he told him he was a bot and that gave him an idea.",
+  "Did you know that my favorite Pokemon is Zoroark",
+  "Did you know that Goku has perfomed the Kamehameha a total of 97 times throughout all three series.",
+  "Did you know that Bulma goes through 17 different hairstyles throughout the series.",
+  "Did you know that Android 17's real name is Lapis.",
+  "Did you know that Android 18's real name is Lazuli.",
+  "Did you know that In Pokemon stadium, it was revealed how Doduo can learn the move fly. Apparently it just runs really fast and the running motion gives it the power of flight! It just floats there, running in place...",
+  "Did you know that Genesect is a very futuristic Pokemon, and Kabutops is an ancient fossil. However, there are rumors that Genesect is actually a Kabutops that's been modified. Although the two don't share any of the same types and use barely any of the same attacks, there is a definite resemblence, especially when you compare Genesect's head to Kabuto.",
+  "Did you know that Azurill is the only Pokemon that can change it's gender when evolving. When evolving into Marill, Azurill has a 1 in 4 chance of switching genders. Fans have debated whether this is because some amphibians are able to change gender, or that it may have embryonic qualities that mean it hasn't fully formed into a gender yet.",	
+  "Did you know that Although Munna wasn't introduced until generation 5, it is actually referenced in the very first game! A woman standing outside of Rock Tunnel in Pokemon Red and Blue dreams about a chunky pink Pokemon with a floral pattern. Clearly the game makers had Munna in mind even way back then.",
+  "Did you know that The fight between Goku and Frieza took up a total of three and a half hours of screen time, making it the longest fight in anime history.",
+  "Did you know that Goku's mother is a Saiyan known as Gine."
+]
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'ping')) {
@@ -35,6 +48,27 @@ client.on('message', message => {
 	if (message.content.startsWith(prefix + 'help')) {
 	message.channel.send(`Hey ${message.author.username} here's the command list:\n a!xd\n a!hello\n a!ping`);
 
+	}
+});
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'test')) {
+			var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+	const embed = new Discord.RichEmbed()
+  .setTitle("Forshadic #1")
+  .setAuthor("Artie", "https://cdn.discordapp.com/avatars/310089392230498315/483a341d8a0fd9eef1908870ba922f40.jpg")
+  /*
+   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+   */
+  .setColor(0x7401DF)
+  .setDescription(randomAnswer)
+  .setImage("https://cdn.discordapp.com/attachments/372491249221107714/380474283324866561/unknown.png")
+.addField("Types:",
+    "Psychic/Ice.")
+		.setFooter("Description by TSComega.")
+  .setTimestamp()
+  message.channel.send({embed});
+	
 	}
 });
 
