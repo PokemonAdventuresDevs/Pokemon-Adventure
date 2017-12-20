f16c1220a2720262abc7aec3a3a45e77101ed424
PokemonAdventuresDevs <34608835+PokemonAdventuresDevs@users.noreply.github.com>
2017-12-19 23:02:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,21 +117,6 @@ client.on('message', message => {
 		message.channel.send(message.author.toString() + `, Do you want to take Popplio with you?          *-a!accept      -a!deny*`);
 	}
 });
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'test')) {
-	
-message.channel.sendMessage('What tag would you like to see? This will await will be cancelled in 30 seconds. It will finish when you provide a message that goes through the filter the first time.')
- const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-        console.log(collector)
-        collector.on('collect', message => {
-            if (message.content == "See") {
-                message.channel.send("You Want To See Someones Spec OK!");
-            } else if (message.content == "Change") {
-                message.channel.send("You Want To Change Your Spec OK!");
-            }
-        })
-});
 
 
 //Important
