c0af19a06ac1b10c72a0d8e80ca15401420237fe
PokemonAdventuresDevs <34608835+PokemonAdventuresDevs@users.noreply.github.com>
2017-12-20 19:07:19 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,10 +125,10 @@ client.on('message', message => {
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'accept')) {
-		else if(choosestarter = 1){
+	
 	message.channel.send(message.author.toString() + `, You took Popplio with you.`);
 
-	}}
+	}
 });
 //Important
 client.login(process.env.BOT_TOKEN);
